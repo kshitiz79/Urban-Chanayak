a7ba90a6006667442bd2bf7d9c989ccfcba9fc79
Manupriya Nagar <kshitizkumarmaurya@Manupriya-MacBook-Air.local>
2025-06-25 17:09:09 +0530

    dones

--- src/components/Home/Video.jsx
+++ src/components/Home/Video.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Video = () => {
   return (
-    <div className="bg-white w-full h-[600px] px-6 py-10">
+    <div className="bg-white w-full h-full px-6 py-10">
       <div className="rounded-xl overflow-hidden h-full w-full max-w-[2830px] mx-auto shadow-lg">
         <video
           className="w-full h-full object-cover rounded-2xl mb-"
